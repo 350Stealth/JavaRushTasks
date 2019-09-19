6f247be5dc324b2830bc78a665f668be1c913da1
Stealth.main <350.stealth@gmail.com>
2019-09-19 23:36:41 +0300

    add task 1528

--- 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -1,11 +1,8 @@
 package com.javarush.task.task15.task1527;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -18,33 +15,30 @@ public class Solution {
         //add your code here
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
         String giveMeURL = scanner.nextLine();
-        String giveMeParams /*= giveMeURL.substring(giveMeURL.indexOf("?"))*/;
+        String giveMeParams;
         scanner.close();
-//        System.out.println(giveMeURL); //should be removed
-//        System.out.println(giveMeParams); //should be removed
-        String[] params /*= giveMeParams.split("&")*/;
-//        System.out.println(params.toString());
+
+        String[] params;
         URL aURL = null;
         try {
             aURL = new URL(giveMeURL);
-        } catch (MalformedURLException e){
+        } catch (MalformedURLException e) {
             e.printStackTrace();
+            return;
         }
-//        System.out.println(aURL.getQuery());
         giveMeParams = aURL.getQuery();
         params = giveMeParams.split("&");
         ArrayList<String[]> kyesAndValues = new ArrayList<>();
-        for (String s: params
-             ) {
-//            System.out.print(s + " ");
+        for (String s : params
+        ) {
             kyesAndValues.add(s.split("="));
         }
         System.out.println();
         boolean flag = false;
-        for (String[] strings: kyesAndValues
-             ) {
+        for (String[] strings : kyesAndValues
+        ) {
             System.out.print(strings[0] + " ");
-            if (strings[0].contains("obj")){
+            if (strings[0].contains("obj")) {
                 flag = true;
             }
         }
@@ -54,7 +48,7 @@ public class Solution {
         if (flag) {
             for (String[] strings : kyesAndValues
             ) {
-                if (strings.length > 1) {
+                if (strings.length > 1 && strings[0].contains("obj")) {
                     try {
                         double d = Double.parseDouble(strings[1]);
                         alert(d);
