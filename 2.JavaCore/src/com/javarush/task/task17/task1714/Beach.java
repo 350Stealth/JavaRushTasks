441fa2fe1d91c16f59ee58e7ccc25350a206589a
Stealth <350.stealth@gmail.com>
2019-10-18 09:03:33 +0300

    Solved 1714 and add 1715

--- 2.JavaCore/src/com/javarush/task/task17/task1714/Beach.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1714/Beach.java
@@ -4,7 +4,7 @@ package com.javarush.task.task17.task1714;
 Comparable
 */
 
-public class Beach {
+public class Beach implements Comparable<Beach> {
     private String name;      //название
     private float distance;   //расстояние
     private int quality;    //качество
@@ -15,30 +15,41 @@ public class Beach {
         this.quality = quality;
     }
 
-    public String getName() {
+    public synchronized String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public synchronized void setName(String name) {
         this.name = name;
     }
 
-    public float getDistance() {
+    public synchronized float getDistance() {
         return distance;
     }
 
-    public void setDistance(float distance) {
+    public synchronized void setDistance(float distance) {
         this.distance = distance;
     }
 
-    public int getQuality() {
+    public synchronized int getQuality() {
         return quality;
     }
 
-    public void setQuality(int quality) {
+    public synchronized void setQuality(int quality) {
         this.quality = quality;
     }
-
+    
+    @Override
+    public synchronized int compareTo(Beach o) {
+        float first = this.quality * this.distance;
+        float second = o.quality * o.distance;
+        if (first == second) {
+            return 0;
+        } else
+//        return (first > second ? 1 : -1);
+        return Math.round(first - second);
+    }
+    
     public static void main(String[] args) {
 
     }
