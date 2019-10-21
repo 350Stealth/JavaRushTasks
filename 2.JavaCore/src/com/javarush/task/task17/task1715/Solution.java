aff3c37384dc47c4ca875a179e205e403805bf8a
Stealth <350.stealth@gmail.com>
2019-10-21 09:03:52 +0300

    solved 1715 & 1716. Added 1717

--- 2.JavaCore/src/com/javarush/task/task17/task1715/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1715/Solution.java
@@ -10,38 +10,61 @@ import java.util.List;
 public class Solution {
     public static DrugsController drugsController = new DrugsController();
     public static boolean isStopped = false;
-
+    
     public static void main(String[] args) throws InterruptedException {
         Thread apteka = new Thread(new Apteka());
         Thread man = new Thread(new Person(), "Мужчина");
         Thread woman = new Thread(new Person(), "Женщина");
-
+        
         apteka.start();
         man.start();
         woman.start();
-
+        
         Thread.sleep(1000);
         isStopped = true;
     }
-
-    public static class Apteka {
-
+    
+    public static class Apteka implements Runnable {
+        @Override
+        public void run() {
+            while (!isStopped) {
+//                synchronized (this) {
+                    drugsController.buy(getRandomDrug(), getRandomCount());
+//                }
+               /* try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }*/
+               waitAMoment();
+               waitAMoment();
+               waitAMoment();
+            }
+        }
     }
-
-    public static class Person {
-
+    
+    public static class Person implements Runnable {
+        @Override
+        public void run() {
+            while (!isStopped) {
+//                synchronized (this){
+                    drugsController.sell(getRandomDrug(), getRandomCount());
+//                }
+                waitAMoment();
+            }
+        }
     }
-
+    
     public static int getRandomCount() {
         return (int) (Math.random() * 3) + 1;
     }
-
+    
     public static Drug getRandomDrug() {
         int index = (int) ((Math.random() * 1000) % (drugsController.allDrugs.size()));
         List<Drug> drugs = new ArrayList<>(drugsController.allDrugs.keySet());
         return drugs.get(index);
     }
-
+    
     private static void waitAMoment() {
         try {
             Thread.sleep(100);
