f79c6845329ea2639e4f218e2fbfac0f9641e2dd
Stealth <350.stealth@gmail.com>
2019-10-22 19:51:58 +0300

    wip 1721

--- 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task17.task1721;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /* 
 Транзакционность
@@ -10,11 +13,42 @@ import java.util.List;
 public class Solution {
     public static List<String> allLines = new ArrayList<String>();
     public static List<String> forRemoveLines = new ArrayList<String>();
-
-    public static void main(String[] args) {
-    }
-
-    public void joinData() throws CorruptedDataException {
     
+    public static void main(String[] args) throws CorruptedDataException{
+        Scanner scanner = new Scanner(System.in);
+        String firstName = scanner.nextLine();
+        String secondName = scanner.nextLine();
+        
+        scanner.close();
+        try {
+            scanner = new Scanner(new FileReader(firstName));
+            while (scanner.hasNext()) {
+                allLines.add(scanner.nextLine());
+            }
+            scanner.close();
+            scanner = new Scanner(new FileReader(secondName));
+            while (scanner.hasNext()) {
+                forRemoveLines.add(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        joinData();
+    }
+    
+    public static void joinData() throws CorruptedDataException {
+        
+        if (allLines.containsAll(forRemoveLines)) {
+            allLines.removeAll(forRemoveLines);
+        } else {
+            allLines.clear();
+            throw new CorruptedDataException();
+        }
+        /*for (String line : forRemoveLines
+        ) {
+            if (allLines.containsAll())
+        }*/
     }
 }
