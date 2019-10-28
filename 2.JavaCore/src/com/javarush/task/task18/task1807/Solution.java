3b34aac790df165b5b02fe4d2c792f36cf85ea1a
Stealth <anubis.delta@gmail.com>
2019-10-28 22:21:20 +0300

    Solved 1807

--- 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
@@ -4,7 +4,25 @@ package com.javarush.task.task18.task1807;
 Подсчет запятых
 */
 
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        scanner.close();
+    
+        FileInputStream inputStream = new FileInputStream(fileName);
+        int buff;
+        int count = 0;
+        while (inputStream.available() > 0) {
+            buff = inputStream.read();
+            if ((char) buff == ',') {
+                count++;
+            }
+        }
+        inputStream.close();
+        System.out.println(count);
     }
 }
