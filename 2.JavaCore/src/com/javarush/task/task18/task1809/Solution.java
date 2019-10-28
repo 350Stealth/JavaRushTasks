f75b067e72df48e4bff3d4eefcb9863dbae7c492
Stealth <350.stealth@gmail.com>
2019-10-28 19:54:12 +0300

    wip 1810

--- 2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
@@ -4,8 +4,29 @@ package com.javarush.task.task18.task1809;
 Реверс файла
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Scanner;
 
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String fileIn = scanner.nextLine();
+        String fileOut = scanner.nextLine();
+        
+        FileInputStream inputStream = new FileInputStream(fileIn);
+        FileOutputStream outputStream = new FileOutputStream(fileOut);
+        if (inputStream.available() > 0) {
+            byte[] buffer = new byte[inputStream.available()];
+            int count = inputStream.read(buffer);
+            byte[] bufferNew = new byte[count];
+            for (int i = 0; i < count; i++) {
+                bufferNew[i] = buffer[count - i - 1];
+            }
+            outputStream.write(bufferNew, 0, count);
+        }
+    
+        outputStream.close();
+        inputStream.close();
     }
 }
