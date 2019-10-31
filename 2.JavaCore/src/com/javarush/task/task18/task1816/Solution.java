9cd39488ece7c34df00186a5db65c81a7d7e20e8
Stealth <350.stealth@gmail.com>
2019-10-31 19:55:21 +0300

    wip 1816

--- 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
@@ -4,29 +4,29 @@ package com.javarush.task.task18.task1816;
 Английские буквы
 */
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         
+        args = new String[1];
         args[0] = "//home//stealth//IdeaProjects//JavaRushTasks//2.JavaCore//src//com//javarush//task//task18//task1816//in.txt";
-    
-        FileInputStream stream = new FileInputStream(args[0]);
-        Scanner scanner = new Scanner(stream);
         
+        FileInputStream stream = new FileInputStream(args[0]);
         String alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
         int count = 0;
-        while (scanner.hasNextByte()) {
-            String newChar = Byte.toString(scanner.nextByte());
-            if (alphabet.contains(newChar)) {
+        while (stream.available() > 0) {
+            char[] newChar = Character.toChars(stream.read());
+            String line = newChar.toString();
+            if (alphabet.contains(line)) {
                 count++;
             }
         }
+
+//        Character ch = Character.isAlphabetic(count);
+        
         System.out.println(count);
-        scanner.close();
+//        scanner.close();
         stream.close();
     }
 }
