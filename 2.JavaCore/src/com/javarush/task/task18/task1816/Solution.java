12415a37353713fb2022f7ebff3e7fbfe8de015a
Stealth <350.stealth@gmail.com>
2019-11-01 09:04:10 +0300

    Solved 1816 & wip 1817

--- 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
@@ -9,24 +9,18 @@ import java.io.FileInputStream;
 public class Solution {
     public static void main(String[] args) throws Exception {
         
-        args = new String[1];
-        args[0] = "//home//stealth//IdeaProjects//JavaRushTasks//2.JavaCore//src//com//javarush//task//task18//task1816//in.txt";
-        
         FileInputStream stream = new FileInputStream(args[0]);
         String alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
         int count = 0;
         while (stream.available() > 0) {
             char[] newChar = Character.toChars(stream.read());
-            String line = newChar.toString();
+            String line = new String(newChar);
             if (alphabet.contains(line)) {
                 count++;
             }
         }
 
-//        Character ch = Character.isAlphabetic(count);
-        
         System.out.println(count);
-//        scanner.close();
         stream.close();
     }
 }
---
+++ 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task18.task1817;
+
+/* 
+Пробелы
+*/
+
+import java.io.FileInputStream;
+
+public class Solution {
+    public static void main(String[] args) throws Exception{
+        
+        args =new String[1];
+        args[0] = "/home//stealth//IdeaProjects//JavaRushTasks//2.JavaCore//src//com//javarush//task//task18//task1817//in.txt";
+    
+        FileInputStream inputStream = new FileInputStream(args[0]);
+        int fullCount = 0;
+        int spaceCount = 0;
+        while (inputStream.available() > 0){
+            int buff = inputStream.read();
+            fullCount++;
+            if (Character.isWhitespace(buff)) {
+                spaceCount++;
+            }
+        }
+        
+        
+    }
+}
