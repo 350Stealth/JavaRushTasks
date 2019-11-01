5ea4f9f207ba72ae77feb591eee5b73b80546f7f
Stealth <350.stealth@gmail.com>
2019-11-01 16:34:35 +0300

    wip 1817

--- 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
@@ -10,7 +10,8 @@ public class Solution {
     public static void main(String[] args) throws Exception{
         
         args =new String[1];
-        args[0] = "/home//stealth//IdeaProjects//JavaRushTasks//2.JavaCore//src//com//javarush//task//task18//task1817//in.txt";
+//        args[0] = "/home//stealth//IdeaProjects//JavaRushTasks//2.JavaCore//src//com//javarush//task//task18//task1817//in.txt";
+        args[0] = "d:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task18\\task1817\\in.txt";
     
         FileInputStream inputStream = new FileInputStream(args[0]);
         int fullCount = 0;
@@ -22,7 +23,8 @@ public class Solution {
                 spaceCount++;
             }
         }
-        
-        
+    
+        System.out.printf("FullCount: %d %nSpaceCount: %d%n", fullCount, spaceCount);
+        System.out.printf("Count: %f", fullCount*1.0/spaceCount);
     }
 }
