9d569a2e87f42b6ebced8ab8f88d3446f713ee17
Stealth <anubis.delta@gmail.com>
2019-11-08 00:42:09 +0300

    wip 1821

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -4,8 +4,24 @@ package com.javarush.task.task18.task1821;
 Встречаемость символов
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class Solution {
+    public static void main(String[] args) throws Exception{
+        
+        // for deleting
+        args = new String[]{"e:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task18\\task1821\\in.txt"};
+        //////////////////////
+    
+        FileInputStream inputStream = new FileInputStream(args[0]);
+        Scanner scanner = new Scanner(inputStream);
+        Map<Character, Integer> map = new HashMap<>();
+        while (scanner.hasNext()) {
+            Byte character = scanner.nextByte();
+            
+        }
     }
 }
