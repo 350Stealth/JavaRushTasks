938761c05097e579bae5c5336490733028a86b28
350.Stealth.Acer <350.stealth@gmail.com>
2019-11-10 02:40:12 +0300

    wip 1821

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -4,6 +4,7 @@ package com.javarush.task.task18.task1821;
 Встречаемость символов
 */
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.util.HashMap;
@@ -11,12 +12,13 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws Exception{
+    
+    public static void main(String[] args) throws Exception {
         
         // for deleting
         args = new String[]{"e:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task18\\task1821\\in.txt"};
         //////////////////////
-    
+        
         FileInputStream inputStream = new FileInputStream(args[0]);
         Scanner scanner = new Scanner(inputStream);
         Map<Character, Integer> map = new HashMap<>();
@@ -25,5 +27,8 @@ public class Solution {
             
         }
         FileReader reader = new FileReader(args[0]);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        reader.read();
+        bufferedReader.read();
     }
 }
