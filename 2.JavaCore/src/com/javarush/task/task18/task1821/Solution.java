38f18a1bce852b9f4f77fed6800350bdb06afc3e
Stealth <anubis.delta@gmail.com>
2019-11-12 01:00:51 +0300

    wip 1821

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -14,17 +14,10 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         
         // for deleting
-//        args = new String[]{"e:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task18\\task1821\\in.txt"};
-        args = new String[]{"d:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task18\\task1821\\in.txt"};
+        args = new String[]{"e:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task18\\task1821\\in.txt"};
+//        args = new String[]{"d:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task18\\task1821\\in.txt"};
         //////////////////////
         
-/*        FileInputStream inputStream = new FileInputStream(args[0]);
-        Scanner scanner = new Scanner(inputStream);
-        Map<Character, Integer> map = new HashMap<>();
-        while (scanner.hasNext()) {
-            Byte character = scanner.nextByte();
-            
-        }*/
         FileReader reader = new FileReader(args[0]);
         BufferedReader bufferedReader = new BufferedReader(reader);
         Map<Character, Integer> map = new HashMap<>();
