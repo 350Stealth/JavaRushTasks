43e21ee356cfec9ec6a85064317031a98750e4b0
Stealth <350.stealth@gmail.com>
2019-11-11 16:58:30 +0300

    wip 1821

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -5,30 +5,50 @@ package com.javarush.task.task18.task1821;
 */
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Solution {
     
     public static void main(String[] args) throws Exception {
         
         // for deleting
-        args = new String[]{"e:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task18\\task1821\\in.txt"};
+//        args = new String[]{"e:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task18\\task1821\\in.txt"};
+        args = new String[]{"d:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task18\\task1821\\in.txt"};
         //////////////////////
         
-        FileInputStream inputStream = new FileInputStream(args[0]);
+/*        FileInputStream inputStream = new FileInputStream(args[0]);
         Scanner scanner = new Scanner(inputStream);
         Map<Character, Integer> map = new HashMap<>();
         while (scanner.hasNext()) {
             Byte character = scanner.nextByte();
             
-        }
+        }*/
         FileReader reader = new FileReader(args[0]);
         BufferedReader bufferedReader = new BufferedReader(reader);
-        reader.read();
-        bufferedReader.read();
+        Map<Character, Integer> map = new HashMap<>();
+        int buff = bufferedReader.read();
+        if (buff != -1) {
+            map.put((char) buff, 1);
+        }
+        while (buff != -1) {
+            if (map.containsKey((char) buff)) {
+                int count = map.get(buff);
+                count++;
+                map.put((char) buff, count);
+            } else {
+                map.put((char) buff, 1);
+            }
+            buff = bufferedReader.read();
+        }
+        bufferedReader.close();
+        reader.close();
+        
+        for (Map.Entry<Character, Integer> item : map.entrySet()
+        ) {
+            System.out.printf("%c - %d", item.getKey(), item.getValue());
+            System.out.println();
+        }
     }
 }
