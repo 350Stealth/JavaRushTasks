ab3584022f9e1a4f8fc4930b83d19120d5b9bec2
Stealth <350.stealth@gmail.com>
2019-11-14 19:31:50 +0300

    Solved 1824

--- 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
@@ -4,7 +4,26 @@ package com.javarush.task.task18.task1824;
 Файлы и исключения
 */
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String fileName = scanner.nextLine();
+            try {
+                FileInputStream inputStream = new FileInputStream(fileName);
+                inputStream.close();
+            } catch (FileNotFoundException e) {
+                System.out.println(fileName);
+                break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        scanner.close();
     }
 }
