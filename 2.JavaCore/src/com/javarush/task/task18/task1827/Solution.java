b060b69c124578f6f50f85fe3d7f3e7feb8e5202
Stealth <350.stealth@gmail.com>
2019-11-18 19:41:40 +0300

    wip 1827

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -6,6 +6,21 @@ package com.javarush.task.task18.task1827;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-    
+        
+        public static void Create() { // Create
+        
+        }
+        
+        public static void ReadData() { // Read
+        
+        }
+        
+        public static void UpdateData() { // Update
+        
+        }
+        
+        public static void DeleteData() { // Delete
+        
+        }
     }
 }
