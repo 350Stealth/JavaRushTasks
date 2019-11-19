a110463206c3d0474ec3947d7a2f15656dc01dd3
Stealth <350.stealth@gmail.com>
2019-11-19 19:53:31 +0300

    wip 1828

--- 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
@@ -6,10 +6,11 @@ package com.javarush.task.task18.task1828;
 
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         
         /*////
         args = new String[4];
@@ -17,16 +18,30 @@ public class Solution {
         args[1] = "Шорты пляжные красные красивые с блестяшками и рюшечками";
         args[2] = "162.00";
         args[3] = "45";
+        */
+        
+        args = new String[5];
+        args[0] = "-u";
+        args[1] = "198478";
+        args[2] = "Шорты пляжные красные красивые с блестяшками и рюшечками";
+        args[3] = "162.00";
+        args[4] = "45";
+        
+        /*
+        args = new String[2];
+        args[0] = "-d";
+        args[1] = "198478";
+         */
         ////*/
 //        System.out.println(args.length);
 //        System.out.println(args == null);
         if (args != null && args.length != 0) {
-        
+            
             Scanner scanner = new Scanner(System.in);
             String fileName = scanner.nextLine();
             scanner.close();
 //        scanner = new Scanner(fileName);
-        
+            
             switch (args[0]) {
                 case "-c":
                     createData(fileName, args[1], args[2], args[3]);
@@ -35,7 +50,7 @@ public class Solution {
                     readData(fileName);
                     break;
                 case "-u":
-                    updateData(fileName, args[1], args[2], args[3]);
+                    updateData(fileName, args[1], args[2], args[3], args[4]);
                     break;
                 case "-d":
                     deleteData(fileName, args[1], args[2], args[3]);
@@ -68,12 +83,34 @@ public class Solution {
         writer.close();
     }
     
-    public static void readData(String fileName){
+    public static void readData(String fileName) {
     
     }
     
-    public static void updateData(String fileName, String productName, String price, String quantity) {
-    
+    public static void updateData(String fileName, String id, String productName, String price, String quantity) throws Exception {
+        Scanner scanner = new Scanner(new FileInputStream(fileName));
+        int lineID;
+        ArrayList<String> lines = new ArrayList<>();
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            int ID = Integer.parseInt(line.substring(0, 8).trim());
+            if (ID == Integer.parseInt(id)) {
+                String item = fillBySpaces(8, id) + fillBySpaces(30, productName) +
+                    fillBySpaces(8, price) + fillBySpaces(4, quantity);
+                lines.add(item);
+            } else {
+                lines.add(line);
+            }
+        }
+        scanner.close();
+        
+        FileWriter writer = new FileWriter(fileName);
+        for (String item : lines
+        ) {
+            writer.write(item);
+            writer.write(System.lineSeparator());
+        }
+        writer.close();
     }
     
     public static void deleteData(String fileName, String productName, String price, String quantity) {
