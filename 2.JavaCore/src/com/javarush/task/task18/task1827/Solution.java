69b7876d1895568e01441cfa24dcd3b949eca853
Stealth <350.stealth@gmail.com>
2019-11-19 09:08:34 +0300

    wip 1827

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -4,23 +4,54 @@ package com.javarush.task.task18.task1827;
 Прайсы
 */
 
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        scanner.close();
+//        scanner = new Scanner(fileName);
         
-        public static void Create() { // Create
-        
+        switch (args[0]) {
+            case "-c":
+                createData(fileName, args[1], args[2], args[3]);
+                break;
+            case "-r":
+                readData(fileName);
+                break;
+            case "-u":
+                updateData(fileName, args[1], args[2], args[3]);
+                break;
+            case "-d":
+                deleteData(fileName, args[1], args[2], args[3]);
+                break;
         }
         
-        public static void ReadData() { // Read
-        
+    }
+    
+    public static void createData(String fileName, String productName, String price, String quantity) {
+        Scanner scanner = new Scanner(fileName);
+        int maxID = 0;
+        while (scanner.hasNext()) {
+            String item = scanner.nextLine();
+            int ID = Integer.parseInt(item.substring(0, 7));
+            if (ID > maxID) {
+                maxID = ID;
+            }
         }
-        
-        public static void UpdateData() { // Update
-        
-        }
-        
-        public static void DeleteData() { // Delete
-        
-        }
+        scanner.close();
+    }
+    
+    public static void readData(String fileName){
+    
+    }
+    
+    public static void updateData(String fileName, String productName, String price, String quantity) {
+    
+    }
+    
+    public static void deleteData(String fileName, String productName, String price, String quantity) {
+    
     }
 }
