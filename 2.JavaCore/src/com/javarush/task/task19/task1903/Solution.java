27b66f37f383df1da17519cd24b78d8391b86189
Stealth <anubis.delta@gmail.com>
2019-11-21 00:12:57 +0300

    Solved 1903

--- 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -21,36 +21,43 @@ public class Solution {
     }
 
     public static class IncomeDataAdapter implements Customer, Contact{
-        private IncomeData incomeData;
+        private IncomeData data;
     
-        public IncomeDataAdapter(IncomeData incomeData) {
-            this.incomeData = incomeData;
+        public IncomeDataAdapter(IncomeData data) {
+            this.data = data;
         }
     
         @Override
         public String getCompanyName() {
-            return incomeData.getCompany();
+            return data.getCompany();
         }
     
         @Override
         public String getCountryName() {
-            return countries.get(incomeData.getCountryCode());
+            return countries.get(data.getCountryCode());
         }
     
         @Override
         public String getName() {
-            return String.format("%s, %s", incomeData.getContactLastName(), incomeData.getContactFirstName());
+            return String.format("%s, %s", data.getContactLastName(), data.getContactFirstName());
         }
     
         @Override
         public String getPhoneNumber() {
-            String phoneCode = String.valueOf(incomeData.getCountryPhoneCode());
-            String phoneNumber = String.valueOf(incomeData.getPhoneNumber());
-            String phoneGSMCode = phoneNumber.substring(0, 2);
-            String numGroup1 = phoneNumber.substring(2, 5);
-            String numGroup2 = phoneNumber.substring(5, 7);
-            String numGroup3 = phoneNumber.substring(7);
-            return String.format("+%s(0%s)%s-%s-%s", phoneCode, phoneGSMCode, numGroup1, numGroup2, numGroup3);
+            /*int phoneCode = data.getCountryPhoneCode();
+            int phoneNumber = data.getPhoneNumber();
+            int phoneGSMCode = phoneNumber / 10000000;
+            int numGroup1 = (phoneNumber - phoneGSMCode * 10000000) / 10000;
+            int numGroup2 = (phoneNumber - phoneGSMCode * 10000000 - numGroup1 * 10000) / 100;
+            int numGroup3 = phoneNumber - phoneGSMCode * 10000000 - numGroup1 * 10000 - numGroup2 * 100;
+            return String.format("%+d(%03d)%s-%s-%s", phoneCode, phoneGSMCode, numGroup1, numGroup2, numGroup3);*/
+            int phoneCode = data.getCountryPhoneCode();
+            String phoneNumber = String.format("%010d", data.getPhoneNumber());
+            String phoneGSMCode = phoneNumber.substring(0, 3);
+            String numGroup1 = phoneNumber.substring(3, 6);
+            String numGroup2 = phoneNumber.substring(6, 8);
+            String numGroup3 = phoneNumber.substring(8);
+            return String.format("%+d(%s)%s-%s-%s", phoneCode, phoneGSMCode, numGroup1, numGroup2, numGroup3);
         }
     }
 
--- 2.JavaCore/src/com/javarush/task/task19/task1903/testClass.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/testClass.java
@@ -2,7 +2,7 @@ package com.javarush.task.task19.task1903;
 
 public class testClass {
     public static void main(String[] args) {
-        int number = 501234567;
+        int number = 1501234567;
         String strNum = Integer.toString(number);
         String gsmCode = strNum.substring(0, 2);
         String nums1 = strNum.substring(2, 5);
@@ -10,5 +10,13 @@ public class testClass {
         String nums3 = strNum.substring(7);
     
         System.out.println(String.format("(0%s)%s-%s-%s", gsmCode, nums1, nums2, nums3));
+    
+        int phoneCode = 38;
+        int phoneNumber = number;
+        int phoneGSMCode = phoneNumber / 10000000;
+        int numGroup1 = (phoneNumber - phoneGSMCode * 10000000) / 10000;
+        int numGroup2 = (phoneNumber - phoneGSMCode * 10000000 - numGroup1 * 10000) / 100;
+        int numGroup3 = phoneNumber - phoneGSMCode * 10000000 - numGroup1 * 10000 - numGroup2 * 100;
+        System.out.println(String.format("%+d(%03d)%d-%d-%d", phoneCode, phoneGSMCode, numGroup1, numGroup2, numGroup3));
     }
 }
