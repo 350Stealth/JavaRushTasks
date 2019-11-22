e7719f68a554f0dd3d1ca71986a864d2bbdcdfd6
Stealth <350.stealth@gmail.com>
2019-11-22 15:55:49 +0300

    Solved 1905

--- 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
@@ -1,6 +1,5 @@
 package com.javarush.task.task19.task1905;
 
-import java.sql.RowId;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,14 +8,14 @@ import java.util.Map;
 */
 
 public class Solution {
-    public static Map<String,String> countries = new HashMap<String,String>();
+    public static Map<String, String> countries = new HashMap<String, String>();
     
     static {
-        countries.put("Ukraine", "UA");
-        countries.put("Russia", "RU");
-        countries.put("Canada", "CA");
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
     }
-
+    
     public static void main(String[] args) {
         
         /*////
@@ -51,9 +50,9 @@ public class Solution {
         System.out.println(adapter.getContactLastName());
         System.out.println(adapter.getContactFirstName());
         ////*/
-
+        
     }
-
+    
     public static class DataAdapter implements RowItem {
         private Customer customer;
         private Contact contact;
@@ -62,17 +61,25 @@ public class Solution {
             this.customer = customer;
             this.contact = contact;
         }
-    
+        
         @Override
         public String getCountryCode() {
-            return countries.get(customer.getCountryName());
+            String line = "";
+            for (Map.Entry<String, String> item : countries.entrySet()
+            ) {
+                if (item.getValue().equals(customer.getCountryName())) {
+                    line = item.getKey();
+                    break;
+                }
+            }
+            return line;
         }
-    
+        
         @Override
         public String getCompany() {
             return customer.getCompanyName();
         }
-    
+        
         @Override
         public String getContactFirstName() {
             String[] names = contact.getName().split("\\W");
@@ -82,35 +89,41 @@ public class Solution {
 //            }                                   //
             return names[2];
         }
-    
+        
         @Override
         public String getContactLastName() {
             String[] names = contact.getName().split("\\W");
             return names[0];
         }
-    
+        
         @Override
         public String getDialString() {
             String number = contact.getPhoneNumber().replaceAll("\\D", "");
             return String.format("callto://+%s", number);
         }
     }
-
+    
     public static interface RowItem {
         String getCountryCode();        //For example: UA
+        
         String getCompany();            //For example: JavaRush Ltd.
+        
         String getContactFirstName();   //For example: Ivan
+        
         String getContactLastName();    //For example: Ivanov
+        
         String getDialString();         //For example: callto://+380501234567
     }
-
+    
     public static interface Customer {
         String getCompanyName();        //For example: JavaRush Ltd.
+        
         String getCountryName();        //For example: Ukraine
     }
-
+    
     public static interface Contact {
         String getName();               //For example: Ivanov, Ivan
+        
         String getPhoneNumber();        //For example: +38(050)123-45-67 or +3(805)0123-4567 or +380(50)123-4567 or ...
     }
 }
