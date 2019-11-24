c9590d1f36b7981adc7b9b28a5ba31c7db9bdb73
Stealth <anubis.delta@gmail.com>
2019-11-24 23:59:16 +0300

    wip 1907

--- 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
@@ -4,8 +4,29 @@ package com.javarush.task.task19.task1907;
 Считаем слово
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
     
+        FileReader fileReader = new FileReader(fileName);
+        Scanner scanner = new Scanner(fileReader);
+        String line;
+        int count = 0;
+        while (scanner.hasNext()) {
+            line = scanner.next();
+            if (line.matches("")) {
+                count++;
+            }
+        }
+        scanner.close();
+        fileReader.close();
+        System.out.println(count);
     }
 }
