c6e0dc36e6f725c8d77d13ed6c50466eae413281
Stealth <350.stealth@gmail.com>
2019-11-28 17:03:57 +0300

    solved 1908 & add 1909

--- 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
@@ -10,27 +10,33 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) throws Exception{
     
-        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
-        String testLineIn = scanner.readLine();
-        String testLineOut =scanner.readLine();
-        scanner.close();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String testLineIn = bufferedReader.readLine();
+        String testLineOut =bufferedReader.readLine();
+        bufferedReader.close();
         
         ////
 //        String testLineIn = "/home/stealth/IdeaProjects/JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1908/in.txt";
 //        String testLineOut = "/home/stealth/IdeaProjects/JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1908/out.txt";
+//        String testLineIn = "d:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1908\\in2.txt";
+//        String testLineOut = "d:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1908\\out.txt";
         ////
     
     
         FileReader fileReader = new FileReader(testLineIn);
         BufferedReader reader = new BufferedReader(fileReader);
+        Scanner scanner;
         String line = "";
         char[] buff = new char[1];
         while (reader.ready()){
-            reader.read(buff);
-            reader.readLine();
-            String symbol = new String(buff);
-            if (symbol.matches("[\\d\\s]")) {
-                line = line + symbol;
+//            reader.read(buff);
+            String nextLine = reader.readLine();
+            scanner = new Scanner(nextLine);
+            while (scanner.hasNext()) {
+                String symbol = scanner.next();//new String(buff);
+                if (symbol.matches("\\d+\\s?")) {
+                    line = line + symbol + " ";
+                }
             }
         }
         
@@ -40,6 +46,7 @@ public class Solution {
         FileWriter writer = new FileWriter(testLineOut);
         BufferedWriter bufferedWriter = new BufferedWriter(writer);
         bufferedWriter.write(line);
+//        System.out.println(line);
         bufferedWriter.close();
         writer.close();
     }
