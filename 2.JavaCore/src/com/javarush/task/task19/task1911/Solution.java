5a8ba619cb3609b81c9674e39f0d541c69525d7e
Stealth <350.stealth@gmail.com>
2019-11-29 17:18:28 +0300

    wip 1911

--- 2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
@@ -4,11 +4,15 @@ package com.javarush.task.task19.task1911;
 Ридер обертка
 */
 
+import java.io.PrintStream;
+
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
-
+        PrintStream concoleStream = System.out; // default stream
+        
+        
     }
 
     public static class TestString {
