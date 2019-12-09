ca0e71a312e85c48c1640fe075c4a4212535089d
Stealth <350.stealth@gmail.com>
2019-12-09 20:43:18 +0300

    ForceSwitch wip

--- WorkTasks/src/com/starccm/viamjava/task/ForceTableSwitch.java
+++ WorkTasks/src/com/starccm/viamjava/task/ForceTableSwitch.java
@@ -9,6 +9,7 @@ public class ForceTableSwitch {
         BufferedReader reader;
         Scanner scanner;
         BufferedWriter writer;
+        BufferedWriter writerMirror;
         final String COMMA = ",";
         
         System.out.println("Enter the file name:");
@@ -16,9 +17,12 @@ public class ForceTableSwitch {
             reader = new BufferedReader(new InputStreamReader(System.in));
             String fileName = reader.readLine();
             String fileNameNew;
+            String fileNameNewMir;
             
             if (fileName.length() > 4) {
-                fileNameNew = fileName.substring(0, fileName.length() - 4) + " mod.csv";
+                String buff = fileName.substring(0, fileName.length() - 4);
+                fileNameNew = buff + " mod.csv";
+                fileNameNewMir = buff + " mod Z mirror.csv";
             } else {
                 System.out.println("Filename is too short!");
                 return;
@@ -27,12 +31,16 @@ public class ForceTableSwitch {
             reader.close();
             System.out.println("The name of the new file is:");
             System.out.println(fileNameNew);
+            System.out.println("The name of the new mirror file is:");
+            System.out.println(fileNameNewMir);
             
             File file = new File(fileName);
             scanner = new Scanner(file).useDelimiter(COMMA);
             writer = new BufferedWriter(new FileWriter(fileNameNew));
+            writerMirror = new BufferedWriter(new FileWriter(fileNameNewMir));
             
             String line;
+            String lineZMirror;
             String[] strings;
             String X, Y, Z, Pa;
             int counter = 5;
@@ -47,15 +55,19 @@ public class ForceTableSwitch {
                     Y = strings[2];
                     Z = strings[3];
                     line = X + COMMA + Y + COMMA + Z + COMMA + Pa;
+                    lineZMirror = X + COMMA + Y + COMMA + Z + COMMA + Pa;
     
                     writer.write(line);
                     writer.newLine();
+                    writerMirror.write(lineZMirror);
+                    writerMirror.newLine();
                     counter = 0;
                 } else
                     counter++;
             }
             
             writer.close();
+            writerMirror.close();
             scanner.close();
     
             System.out.println("All done!");
