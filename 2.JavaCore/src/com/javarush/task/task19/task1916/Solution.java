312b4537d347cdccf7c4f3c40729e146a91f5ba4
Stealth <350.stealth@gmail.com>
2020-01-15 17:51:40 +0300

    wip 1916

--- 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -16,21 +16,24 @@ public class Solution {
     
     public static void main(String[] args) throws Exception {
         BufferedReader consolReader = new BufferedReader(new InputStreamReader(System.in));
-        String fileIn_1 = consolReader.readLine();
-        String fileIn_2 = consolReader.readLine();
+//        String fileIn_1 = consolReader.readLine();
+//        String fileIn_2 = consolReader.readLine();
         consolReader.close();
 
 //        String fileIn_1 = "e:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1916\\in1.txt";
 //        String fileIn_2 = "e:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1916\\in2.txt";
-        
+
+        String fileIn_1 = "d:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1916\\in1.txt";
+        String fileIn_2 = "d:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1916\\in2.txt";
+    
         FileReader fileReader_1 = new FileReader(new File(fileIn_1));
         BufferedReader reader_1 = new BufferedReader(fileReader_1);
         FileReader fileReader_2 = new FileReader(new File(fileIn_2));
         BufferedReader reader_2 = new BufferedReader(fileReader_2);
-        
+    
         ArrayList<String> linesFromFF = new ArrayList<>();
         ArrayList<String> linesFromSF = new ArrayList<>();
-        
+    
         while (reader_1.ready()) {
             linesFromFF.add(reader_1.readLine());
         }
@@ -47,27 +50,54 @@ public class Solution {
         int i = 0;
         int j = 0;
         
-        while (i < linesFromFF.size() && j < linesFromSF.size()) {
+        while (/*i < linesFromFF.size() && j < linesFromSF.size()*/true) {
             if (linesFromFF.get(i).equals(linesFromSF.get(j))) {
                 lines.add(new LineItem(Type.SAME, linesFromFF.get(i)));
 //                System.out.println("SAME " + linesFromFF.get(i));
-                i++;
-                j++;
+//                i++;
+                if (i + 1 < linesFromFF.size()) i++;
+//                j++;
+                if (j + 1 < linesFromSF.size()) j++;
             } else if ((i + 1) < linesFromFF.size() && linesFromFF.get(i + 1).equals(linesFromSF.get(j))) {
                 lines.add(new LineItem(Type.REMOVED, linesFromFF.get(i)));
 //                System.out.println("REMOVE " + linesFromFF.get(i));
                 lines.add(new LineItem(Type.SAME, linesFromFF.get(i + 1)));
 //                System.out.println("SAME " + linesFromFF.get(i + 1));
-                j++;
-                i += 2;
+//                j++;
+                if (j + 1 < linesFromSF.size()) {
+                    j++;
+                }
+//                i += 2;
+                if (i + 2 < linesFromFF.size()) {
+                    i += 2;
+                } else {
+                    i++;
+                }
             } else if ((j + 1) < linesFromSF.size() && linesFromSF.get(j + 1).equals(linesFromFF.get(i))) {
                 lines.add(new LineItem(Type.ADDED, linesFromSF.get(j)));
 //                System.out.println("ADDED " + linesFromSF.get(j));
                 lines.add(new LineItem(Type.SAME, linesFromFF.get(i)));
 //                System.out.println("SAME " + linesFromFF.get(i));
-                i++;
-                j += 2;
+//                i++;
+                if (i + 1 < linesFromFF.size()) {
+                    i++;
+                }
+//                j += 2;
+                if (j + 2 < linesFromSF.size()) {
+                    j += 2;
+                } else {
+                    j++;
+                }
             }
+            if (i + 1 >= linesFromFF.size() && j + 1 >= linesFromSF.size()) {
+                break;
+            }
+        }
+    
+        for (LineItem item: lines
+             ) {
+            System.out.printf("%s: %s", item.line, item.type);
+            System.out.println();
         }
         
     }
