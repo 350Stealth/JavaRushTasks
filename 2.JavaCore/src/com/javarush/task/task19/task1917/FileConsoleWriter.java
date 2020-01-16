de659a0c4e7316e4fd80448008d7248d8a51f1f3
Stealth <anubis.delta@gmail.com>
2020-01-16 19:54:18 +0300

    wip 1917

--- 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
@@ -4,9 +4,43 @@ package com.javarush.task.task19.task1917;
 Свой FileWriter
 */
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileConsoleWriter {
     public static void main(String[] args) {
 
     }
-
+    
+    private FileWriter fileWriter;
+    
+    public FileConsoleWriter(FileWriter fileWriter) {
+        this.fileWriter = fileWriter;
+    }
+    
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        fileWriter.write(cbuf, off, len);
+        String line = String.valueOf(cbuf, off, len);
+        System.out.println(line);
+    }
+    
+    public void write(int c) throws IOException {
+        fileWriter.write(c);
+        System.out.println(c);
+    }
+    
+    public void write(String str) throws IOException {
+        fileWriter.write(str);
+        System.out.println(str);
+    }
+    
+    public void write(String str, int off, int len) throws IOException {
+        fileWriter.write(str, off, len);
+        String line = String.valueOf(str.toCharArray(), off, len);
+        System.out.println(line);
+    }
+    
+    public void write(char[] cbuf) throws IOException {
+    
+    }
 }
