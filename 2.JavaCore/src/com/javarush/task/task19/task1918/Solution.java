182636d0040748dc9d0967052d249b88e6d24d9e
Stealth <anubis.delta@gmail.com>
2020-01-23 19:45:20 +0300

    wip 1918

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -8,7 +8,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
@@ -17,11 +20,12 @@ public class Solution {
         bufferedReader.close();
         
          //input data
-        String fileName = "d:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1918\\in.txt";
+//        String fileName = "d:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1918\\in.txt";
+        String fileName = "/home/stealth/Projects/Java/JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1918/in.txt";
         
         args = new String[] {"span"};
         
-        String regexpression = String.format("<%s>.*?</%s>", args[0], args[0]);
+        String regexpression = String.format("(<%s>)*?(</%s>)", args[0], args[0]);
         System.out.println(regexpression);
         String textFromFile = "";
         Scanner scanner = new Scanner(new FileReader(fileName));
@@ -29,8 +33,19 @@ public class Solution {
             textFromFile += scanner.next();
         }
         scanner.close();
-        String[] strArray = textFromFile.split(String.format("<%s>*?</%s>", args[0], args[0]));
     
+        Pattern pattern = Pattern.compile(regexpression);
+    
+        System.out.println(pattern.pattern());
+        
+//        String[] strArray = textFromFile.split(regexpression);
+        String[] strArray = pattern.split(textFromFile.subSequence(0, textFromFile.length()));
+//        List<String> strArrList = new ArrayList<>();
+        
+//        while (textFromFile.matches(regexpression)) {
+//            strArrList.add(textFromFile.);
+//        }
+        
         System.out.println(textFromFile);
         for (String item: strArray) {
             System.out.println(item);
