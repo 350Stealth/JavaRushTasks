eefb36af18e88f011dc64a7945909d56041fff25
Stealth <350.stealth@gmail.com>
2020-02-11 18:32:29 +0300

    wip 1918

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -8,9 +8,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -59,18 +57,18 @@ public class Solution {
         System.out.println(pattern.pattern());
 
         System.out.println(textFromFile);
-        for (String item : strArrList) {
+        /*for (String item : strArrList) {
             System.out.println(item);
-        }
+        }*/
         for (Integer item: intArrStart) {
             System.out.println(item);
         }
     
         System.out.println("--------");
     
-        for (String item: strArrListEnd) {
+        /*for (String item: strArrListEnd) {
             System.out.println(item);
-        }
+        }*/
         for (Integer item: intArrEnd) {
             System.out.println(item);
         }
@@ -79,20 +77,93 @@ public class Solution {
         System.out.println(line);
         
         List<String> setOfTags = new ArrayList<>();
-
+//        Map<Integer, String> indexMap = new HashMap<>();
+        List<Integer> indexList = new ArrayList<>();
+        int indexI = 0, indexJ = 0;
+        boolean flagStart = true, flagEnd = true;
+        while (flagStart || flagEnd) {
+            if (intArrStart.get(indexI) < intArrEnd.get(indexJ)) {
+//                indexMap.put(intArrStart.get(indexI), regexpressionStart);
+                indexList.add(intArrStart.get(indexI));
+                if (indexI + 1 < intArrStart.size()) {
+                    indexI++;
+                } else {
+                    flagStart = false;
+                }
+            } else {
+//                indexMap.put(intArrEnd.get(indexJ), regexpressionEnd);
+                indexList.add(intArrEnd.get(indexJ));
+                if (indexJ + 1 < intArrEnd.size()) {
+                    indexJ++;
+                } else {
+                    flagEnd = false;
+                }
+            }
+            if (indexI + 1 >= intArrStart.size()) {
+                flagStart = false;
+//                indexI--;
+            }
+            if (indexJ + 1 >= intArrEnd.size()) {
+                flagEnd = false;
+//                indexJ--;
+            }
+        }
+    
+       /* for (Map.Entry<Integer, String> item: indexMap.entrySet()) {
+            System.out.println(item.getKey() + " " + item.getValue());
+        }*/
+        for (Integer item: indexList) {
+            System.out.println(item);
+        }
+        
         int startIndex = 0, endIndex = 0;
-        while (true) {
-            int startPos = intArrStart.get(startIndex);
-            int endPos = intArrEnd.get(endIndex) + 7;
-            if (startPos > endPos) {
-                endIndex++;
-                continue;                               //check for free ends
+        
+        /*while (!intArrStart.isEmpty() && !intArrEnd.isEmpty()) {
+            int i = 0, j = 0, k = 0;
+            startIndex = intArrStart.get(i);
+            i++;
+            do {
+//                endIndex = intArrEnd.get(j);
+                if (i < intArrStart.size()) {
+                    if (intArrStart.get(i) < intArrEnd.get(j)) {
+                        i++;
+                        k++;
+                    } else {
+                        k--;
+                    }
+                }
+            } while (k > 0 || j < intArrEnd.size());
+            endIndex = intArrEnd.get(j) + 7;
+            setOfTags.add(textFromFile.substring(startIndex, endIndex));
+            intArrStart.remove(i);
+            intArrEnd.remove(j);
+        }*/
+        int i = 0, j = 0, k = 0;
+        while (i < indexList.size()) {
+//            j = i + 1;
+            if (intArrStart.contains(indexList.get(i))) {
+                startIndex = indexList.get(i);
+            } else {
+                i++;
+                continue;
             }
-            while (startPos > endPos || (startIndex + 1) < intArrStart.size()) {
-            
+            k++;
+            while (k != 0 || j + 1 < indexList.size()) {
+                j++;
+                if (intArrEnd.contains(indexList.get(j))){
+                    k--;
+                } else {
+                    k++;
+                }
+                if (k == 0) {
+                    endIndex = indexList.get(j) + 7;
+                }
             }
-//            while (startPos)
-            break;
+            setOfTags.add(textFromFile.substring(startIndex, endIndex));
+        }
+    
+        for (String item: setOfTags) {
+            System.out.println(item);
         }
     }
 }
