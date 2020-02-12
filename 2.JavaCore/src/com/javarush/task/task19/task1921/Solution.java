ea238540cdace53f1619ba092d990eb9c8f48236
Stealth-PC <anubis.delta@yandex.ru>
2020-02-12 23:55:15 +0300

    wip 1921

--- 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task19.task1921;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,9 +13,9 @@ import java.util.Scanner;
 
 public class Solution {
     public static final List<Person> PEOPLE = new ArrayList<Person>();
-
-    public static void main(String[] args) throws IOException {
     
+    public static void main(String[] args) throws IOException {
+
 //        String fileName = args[0];
         String fileName = "D:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1921\\in.txt";
         
@@ -27,9 +28,20 @@ public class Solution {
         }
         scanner.close();
         fileReader.close();
-    
-        for (String item: lines) {
-            item.
+        
+        for (String item : lines) {
+            String[] words = item.split("\\s");
+            int wordSize = words.length;
+            StringBuilder name = new StringBuilder();
+            for (int i = 0; i < wordSize - 2; i++) {
+                name.append(String.format("%s ", words[i]));
+            }
+            int date1 = Integer.parseInt(words[wordSize - 3]);
+            int date2 = Integer.parseInt(words[wordSize - 2]);
+            int date3 = Integer.parseInt(words[wordSize - 1]);
+            Date dateObject = new Date(date1, date2, date3);
+            
+            PEOPLE.add(new Person(name.toString(), dateObject));
         }
     }
 }
