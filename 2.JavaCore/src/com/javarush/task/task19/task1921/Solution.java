6d49a9aaa406ed7455cb39bd97beea703f926cb1
Stealth <350.stealth@gmail.com>
2020-02-12 21:29:39 +0300

    wip 1921

--- 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task19.task1921;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /* 
 Хуан Хуанович
@@ -10,7 +13,23 @@ import java.util.List;
 public class Solution {
     public static final List<Person> PEOPLE = new ArrayList<Person>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+    
+//        String fileName = args[0];
+        String fileName = "D:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1921\\in.txt";
         
+        List<String> lines = new ArrayList<>();
+        
+        FileReader fileReader = new FileReader(fileName);
+        Scanner scanner = new Scanner(fileReader);
+        while (scanner.hasNextLine()) {
+            lines.add(scanner.nextLine());
+        }
+        scanner.close();
+        fileReader.close();
+    
+        for (String item: lines) {
+            item.
+        }
     }
 }
