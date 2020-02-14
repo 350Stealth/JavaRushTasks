3300c6e74e761ccc7dc9ce93170ee8d78160beea
Stealth <350.stealth@gmail.com>
2020-02-14 17:24:01 +0300

    1922

--- 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -1,10 +1,14 @@
 package com.javarush.task.task19.task1922;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /* 
 Ищем нужные строки
@@ -27,6 +31,28 @@ public class Solution {
         
         String fileName = "D:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1922\\in.txt";
         
+        String regExpression = String.format("\\b[(%s)(%s)(%s)]\\b", words.get(0), words.get(1), words.get(2));
+        Pattern pattern = Pattern.compile(regExpression);
+        List<String> lines = new ArrayList<>();
         
+        FileReader fileReader = new FileReader(fileName);
+        Scanner scanner = new Scanner(fileReader);
+        while (scanner.hasNextLine()) {
+            lines.add(scanner.nextLine());
+        }
+        scanner.close();
+        fileReader.close();
+        
+        List<String> wordGroups = new ArrayList<>();
+        for (String item: lines) {
+            Matcher mText = pattern.matcher(item);
+            while (mText.find()) {
+                wordGroups.add(mText.group());
+            }
+            if (wordGroups.size() == 2) {
+                System.out.println(item);
+            }
+            wordGroups.clear();
+        }
     }
 }
