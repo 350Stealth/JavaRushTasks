da48e4d8412dfec1d25feef3501ee987fb44b160
Stealth <350.stealth@gmail.com>
2020-02-17 12:38:29 +0300

    Solved 1922 & add 1923

--- 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -7,8 +7,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /* 
 Ищем нужные строки
@@ -26,17 +24,16 @@ public class Solution {
     public static void main(String[] args) throws IOException {
     
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//        String fileName = bufferedReader.readLine();
+        String fileName = bufferedReader.readLine();
         bufferedReader.close();
-        
+
 //        String fileName = "D:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1922\\in.txt";
-        String fileName = "/home/stealth/Projects/Java/JavaRush/JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1922/in.txt";
-        
-        String regExpression = String.format("[(%s)(%s)(%s)]", words.get(0), words.get(1), words.get(2));
-//        String regExpression = String.format("\\b[(%s)]\\b", words.get(0));
-        Pattern pattern = Pattern.compile(regExpression);
+//        String fileName = "/home/stealth/Projects/Java/JavaRush/JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1922/in.txt";
+    
+        String regExpression = "\\s+";
+//        Pattern pattern = Pattern.compile(regExpression);
         List<String> lines = new ArrayList<>();
-        
+    
         FileReader fileReader = new FileReader(fileName);
         Scanner scanner = new Scanner(fileReader);
         while (scanner.hasNextLine()) {
@@ -44,12 +41,19 @@ public class Solution {
         }
         scanner.close();
         fileReader.close();
-        
+    
         List<String> wordGroups = new ArrayList<>();
         for (String item: lines) {
-            Matcher mText = pattern.matcher(item);
-            while (mText.find()) {
-                wordGroups.add(mText.group());
+            String[] splitString = item.split(regExpression);
+            for (String word : splitString) {
+                /*if (word.equals(words.get(0)) || word.equals(words.get(1)) || word.equals(words.get(2))) {
+                    wordGroups.add(word);
+                }*/
+                for (String string : words) {
+                    if (word.equals(string)) {
+                        wordGroups.add(word);
+                    }
+                }
             }
             if (wordGroups.size() == 2) {
                 System.out.println(item);
