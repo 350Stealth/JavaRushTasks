6c64ae0d47dd2703fead6b6e883de9fc10c82a69
Stealth <anubis.delta@gmail.com>
2020-02-18 19:34:48 +0300

    Solved 1927 & add 1928

--- 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -6,22 +6,41 @@ package com.javarush.task.task19.task1927;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class Solution {
     public static TestString testString = new TestString();
-
-    public static void main(String[] args) {
-        PrintStream concoleStream = System.out; //default stream
     
+    public static void main(String[] args) {
+        String advertise = "JavaRush - курсы Java онлайн";
+        
+        PrintStream concoleStream = System.out; //default stream
+        
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         
         PrintStream stream = new PrintStream(outputStream);
         
         System.setOut(stream);
         
+        testString.printSomething();
         
+        System.setOut(concoleStream);
+        
+        String input = outputStream.toString();
+        
+        boolean flag = false;
+        Scanner scanner = new Scanner(input);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+            if (flag) {
+                System.out.println(advertise);
+            }
+            flag = !flag;
+        }
+        scanner.close();
     }
-
+    
     public static class TestString {
         public void printSomething() {
             System.out.println("first");
