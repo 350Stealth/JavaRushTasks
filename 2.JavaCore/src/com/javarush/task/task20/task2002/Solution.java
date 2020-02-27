46ce8696c9cfb3eebe8fb4098caf38c9392d534b
Stealth-PC <anubis.delta@yandex.ru>
2020-02-28 02:13:33 +0300

    Solved 2002 & add 2003

--- 2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
@@ -1,10 +1,10 @@
 package com.javarush.task.task20.task2002;
 
 import java.io.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 /* 
 Читаем и пишем в файл: JavaRush
@@ -18,26 +18,35 @@ public class Solution {
             File yourFile = File.createTempFile(fileName, null);
             OutputStream outputStream = new FileOutputStream(yourFile);
             InputStream inputStream = new FileInputStream(yourFile);
-
+            
             JavaRush javaRush = new JavaRush();
             //initialize users field for the javaRush object here - инициализируйте поле users для объекта javaRush тут
             User ivanov = new User();
             ivanov.setFirstName("Ivan");
             ivanov.setLastName("Ivanov");
-            ivanov.setBirthDate(new Date());
+            ivanov.setBirthDate(new Date(83, 8, 20));
             ivanov.setMale(true);
             ivanov.setCountry(User.Country.RUSSIA);
+            User petrova = new User();
+            petrova.setFirstName("Julia");
+            petrova.setLastName("Petrova");
+            petrova.setBirthDate(new Date(91, 05, 12));
+            petrova.setMale(false);
+            petrova.setCountry(User.Country.UKRAINE);
             javaRush.users.add(ivanov);
+            javaRush.users.add(petrova);
             javaRush.save(outputStream);
             outputStream.flush();
-
+            
             JavaRush loadedObject = new JavaRush();
-//            loadedObject.load(inputStream);
+            loadedObject.load(inputStream);
             //here check that the javaRush object is equal to the loadedObject object - проверьте тут, что javaRush и loadedObject равны
-
+            
+            System.out.println(javaRush.equals(loadedObject));
+            
             outputStream.close();
             inputStream.close();
-
+            
         } catch (IOException e) {
             //e.printStackTrace();
             System.out.println("Oops, something is wrong with my file");
@@ -46,53 +55,69 @@ public class Solution {
             System.out.println("Oops, something is wrong with the save/load method");
         }
     }
-
+    
     public static class JavaRush {
         public List<User> users = new ArrayList<>();
-
+        
         public void save(OutputStream outputStream) throws Exception {
             //implement this method - реализуйте этот метод
-            String fileNameTest = "D:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task20\\task2002\\out.txt"; //------------
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
-            BufferedWriter writerTest = new BufferedWriter(new FileWriter(fileNameTest)); //-------------
             boolean flag = false;
-            for (User item: this.users) {
+            for (User item : this.users) {
                 if (flag) {
                     writer.write("\n");
-                    writerTest.write("\n");
                 } else {
                     flag = true;
                 }
                 writer.write(String.format("%s %s\n", item.getFirstName(), item.getLastName()));
-                writerTest.write(String.format("%s %s\n", item.getFirstName(), item.getLastName())); //-----------
-                writer.write(String.format("%s\n",item.getBirthDate().toString()));
-                writerTest.write(String.format("%s\n",item.getBirthDate().toString())); //-----------
-                writer.write(item.isMale()? "Male" : "Female");
-                writerTest.write(item.isMale()? "Male" : "Female"); //----------
+                writer.write(String.format("%s\n", item.getBirthDate().getTime()));
+                writer.write(item.isMale() ? "Male" : "Female");
                 writer.write("\n");
-                writerTest.write("\n"); //------------
                 writer.write(String.format("%s", item.getCountry().getDisplayName()));
-                writerTest.write(String.format("%s", item.getCountry().getDisplayName())); //-----------
             }
-            writerTest.close(); //-------------
             writer.close();
         }
-
+        
         public void load(InputStream inputStream) throws Exception {
             //implement this method - реализуйте этот метод
+            Scanner scanner = new Scanner(inputStream);
+            while (scanner.hasNextLine()) {
+                User newUser = new User();
+                newUser.setFirstName(scanner.next());
+                newUser.setLastName(scanner.next());
+                long birthDate = scanner.nextLong();
+//                System.out.println(birthDate);
+                newUser.setBirthDate(new Date(birthDate));
+//                System.out.println(newUser.getBirthDate());
+                String sex = scanner.next();
+                newUser.setMale(sex.equals("Male"));
+                String userCountry = scanner.next();
+                switch (userCountry) {
+                    case "Russia":
+                        newUser.setCountry(User.Country.RUSSIA);
+                        break;
+                    case "Ukraine":
+                        newUser.setCountry(User.Country.UKRAINE);
+                        break;
+                    default:
+                        newUser.setCountry(User.Country.OTHER);
+                }
+                users.add(newUser);
+            }
+            scanner.close();
         }
-
+        
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
-
+            
             JavaRush javaRush = (JavaRush) o;
-
+            
             return users != null ? users.equals(javaRush.users) : javaRush.users == null;
-
+            
         }
-
+        
         @Override
         public int hashCode() {
             return users != null ? users.hashCode() : 0;
