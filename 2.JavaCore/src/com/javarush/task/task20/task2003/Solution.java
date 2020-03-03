9e124594d052755def2a9a31958f7f2e013c5ba0
StealthAspire3 <350.stealth@yandex.ru>
2020-03-03 13:00:19 +0300

    Solved 2003

--- 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -13,9 +13,7 @@ public class Solution {
     public /*static*/ void fillInPropertiesMap() throws IOException {
         //implement this method - реализуйте этот метод
         Scanner scanner = new Scanner(System.in);
-//        String fileName = scanner.nextLine();
-//        String fileName = "D:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task20\\task2003\\config.property";
-        String fileName = "D:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task20\\task2003\\cursors.property";
+        String fileName = scanner.nextLine();
         scanner.close();
         InputStream stream = new FileInputStream(fileName);
         try {
@@ -28,18 +26,6 @@ public class Solution {
     
     public /*static*/ void save(OutputStream outputStream) throws Exception {
         //implement this method - реализуйте этот метод
-        /*BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
-        boolean flag = false;
-        for (Map.Entry<String, String> item : properties.entrySet()) {
-            if (flag) {
-                writer.newLine();
-            } else {
-                flag = true;
-            }
-            writer.write(String.format("%s = %s", item.getKey(), item.getValue()));
-            writer.flush();
-        }
-        writer.close();*/
         Properties prop = new Properties();
         for (Map.Entry<String, String> item: properties.entrySet()) {
             prop.setProperty(item.getKey(), item.getValue());
@@ -49,17 +35,6 @@ public class Solution {
     
     public /*static*/ void load(InputStream inputStream) throws Exception {
         //implement this method - реализуйте этот метод
-        /*Scanner scanner = new Scanner(inputStream);
-        while (scanner.hasNextLine()) {
-            String keyLine = scanner.next();
-            if (keyLine.contains("////") || keyLine.contains("#") || keyLine.contains("!") || keyLine.isEmpty()) {
-                continue;
-            }
-            scanner.next();
-            String valueLine = scanner.nextLine();
-            properties.put(keyLine, valueLine);
-        }
-        scanner.close();*/
         Properties prop = new Properties();
         while (inputStream.available() > 0) {
             prop.load(inputStream);
@@ -71,15 +46,6 @@ public class Solution {
     }
     
     public static void main(String[] args) throws IOException {
-//        String fileName = "D:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task20\\task2003\\config.properties";
-        Solution solution = new Solution();
-        solution.fillInPropertiesMap();
-        String fileName = "D:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task20\\task2003\\out.txt";
-        OutputStream outputStream = new FileOutputStream(fileName);
-        try {
-            solution.save(outputStream);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
     }
 }
