2b2b4bb9f42ddfab6a80047d5cc62ded1838e73b
Stealth <350.stealth@gmail.com>
2020-06-05 14:26:40 +0300

    Solved 2020 & add 2021

--- 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -1,10 +1,25 @@
 package com.javarush.task.task20.task2021;
 
-/* 
+import java.io.NotSerializableException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/*
 Сериализация под запретом
 */
 public class Solution implements Serializable {
-    public static class SubSolution extends Solution {
+    public static class SubSolution extends Solution implements Serializable{
+        public SubSolution() {
+        }
+        
+        public void readObject(ObjectInputStream inputStream) throws NotSerializableException{
+            throw new NotSerializableException();
+        }
+        
+        public void writeObject(ObjectOutputStream outputStream) throws NotSerializableException{
+            throw new NotSerializableException();
+        }
     }
 
     public static void main(String[] args) {
