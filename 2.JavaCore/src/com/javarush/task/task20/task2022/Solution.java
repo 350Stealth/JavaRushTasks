d955248e9d2fd56d68c3016349a9a596091d874d
Stealth <350.stealth@gmail.com>
2020-06-09 17:56:59 +0300

    Solved 2022 & add 2023

--- 2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
@@ -6,10 +6,12 @@ import java.io.*;
 Переопределение сериализации в потоке
 */
 public class Solution implements Serializable, AutoCloseable {
-    private FileOutputStream stream;
+    private transient FileOutputStream stream;
+    private String fileName;
 
     public Solution(String fileName) throws FileNotFoundException {
         this.stream = new FileOutputStream(fileName);
+        this.fileName = fileName;
     }
 
     public void writeObject(String string) throws IOException {
@@ -20,12 +22,14 @@ public class Solution implements Serializable, AutoCloseable {
 
     private void writeObject(ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
-        out.close();
+//        out.close();
     }
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-        in.defaultReadObject();
-        in.close();
+        stream = new FileOutputStream(fileName, true);
+        fileName = (String) in.readObject();
+//        in.defaultReadObject();
+//        in.close();
     }
 
     @Override
@@ -34,11 +38,29 @@ public class Solution implements Serializable, AutoCloseable {
         stream.close();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         
         //D:\IdeaProjects\JavaRushTasks\JavaRushTasks\2.JavaCore\src\com\javarush\task\task20\task2022\TestFile.txt
-        Solution sol = new Solution("D:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task20\\task2022\\TestFile.txt");
+        String testFileName = "D:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task20\\task2022\\TestFile.txt";
+        Solution sol = new Solution(testFileName);
         sol.writeObject("Test String");
         
+        //D:\IdeaProjects\JavaRushTasks\JavaRushTasks\2.JavaCore\src\com\javarush\task\task20\task2022\TestFile2.txt
+        String serializeFileName = "D:\\IdeaProjects\\JavaRushTasks\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task20\\task2022\\TestFile2.txt";
+        FileOutputStream fileOutput = new FileOutputStream(serializeFileName);
+        ObjectOutputStream outputStream = new ObjectOutputStream(fileOutput);
+//        sol.writeObject(outputStream);
+        outputStream.writeObject(sol);
+        sol.close();
+        outputStream.close();
+        fileOutput.close();
+        
+        FileInputStream fileInputStream = new FileInputStream(serializeFileName);
+        ObjectInputStream inputStream = new ObjectInputStream(fileInputStream);
+        Solution solNew = (Solution) inputStream.readObject();
+        solNew.writeObject("Test String 2");
+        solNew.close();
+        inputStream.close();
+        fileInputStream.close();
     }
 }
