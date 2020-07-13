9410d8a17f824f1e5f51bfefe5cb54ac0c77d3ad
Stealth-PC <anubis.delta@yandex.ru>
2020-07-13 23:48:37 +0300

    wip 2026

--- 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
@@ -6,36 +6,72 @@ package com.javarush.task.task20.task2026;
 public class Solution {
     public static void main(String[] args) {
         byte[][] a1 = new byte[][]{
-                {1, 1, 0, 0},
-                {1, 1, 0, 0},
-                {1, 1, 0, 0},
-                {1, 1, 0, 1}
+            {1, 1, 0, 0},
+            {1, 1, 0, 0},
+            {1, 1, 0, 0},
+            {1, 1, 0, 1}
         };
         byte[][] a2 = new byte[][]{
-                {1, 0, 0, 1},
-                {0, 0, 0, 0},
-                {0, 0, 0, 0},
-                {1, 0, 0, 1}
+            {1, 0, 0, 1},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {1, 0, 0, 1}
         };
-
+    
+        System.out.println(a1.length);
+        System.out.println(a2.length);
         int count1 = getRectangleCount(a1);
         System.out.println("count = " + count1 + ". Должно быть 2");
         int count2 = getRectangleCount(a2);
         System.out.println("count = " + count2 + ". Должно быть 4");
     }
-
+    
     public static int getRectangleCount(byte[][] a) {
-        int posI = 0,
-            posJ = 0,
-            i = 0,
-            j = 0;
+        int posX, posY,
+            x = 0,
+            y = 0,
+            count = 0,
+            cell;
+        boolean flag = false;
         
         while (true) {
-            
-            
-            if (true) break;
+            cell = a[x][y];
+            if (cell == 1) {
+                count++;
+                posX = x;
+                posY = y;
+                boolean flagX = true,
+                    flagY = true;
+                while (a[posX][posY] == 1 || (flagX && flagY)) {
+                    if (flagX) {
+                        if (posX + 1 >= a.length || a[posX + 1][posY] == 0) {
+                            flagX = false;
+                        } else {
+                            posX++;
+                        }
+                    } else {
+                        if (posY + 1 >= a.length || a[posX][posY + 1] == 0) {
+                            flagY = false;
+                        } else {
+                            posY++;
+                        }
+                    }
+                }
+                for (int i = x; i < posX; i++) {
+                    for (int j = y; j < posY; j++) {
+                        a[i][j] = 0;
+                    }
+                }
+            }
+            x++;
+            if (x >= a.length) {
+                x = 0;
+                y++;
+                if (y >= a.length) flag = true;
+            }
+            if (flag) break;
         }
         
-        return 0;
+        return count;
     }
 }
