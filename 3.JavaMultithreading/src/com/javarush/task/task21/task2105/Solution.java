022a0995fa1208467f4a9953809d0ccf980d01df
Stealth <350.stealth@gmail.com>
2020-09-01 18:34:20 +0300

    Solved 2105 & add 2106

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task21.task2105;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /* 
@@ -14,13 +15,32 @@ public class Solution {
         this.last = last;
     }
 
-    public boolean equals(Object o) {
+    /*public boolean equals(Object o) {
         if (!(o instanceof Solution))
             return false;
         Solution n = (Solution) o;
         return n.first.equals(first) && n.last.equals(last);
+    }*/
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if(!(o instanceof Solution)) return false;
+        
+        Solution solution = (Solution) o;
+        
+        if (!Objects.equals(first, solution.first)) return false;
+        return Objects.equals(last, solution.last);
     }
-
+    
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
+    }
+    
     public static void main(String[] args) {
         Set<Solution> s = new HashSet<>();
         s.add(new Solution("Mickey", "Mouse"));
