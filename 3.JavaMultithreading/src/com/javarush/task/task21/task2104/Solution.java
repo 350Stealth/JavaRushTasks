43014841d2e896a3dd50faedfca122f4baa6762b
Stealth <350.stealth@gmail.com>
2020-09-01 18:23:24 +0300

    Solved 2104 & add 2105

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task21.task2104;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /* 
@@ -14,17 +15,36 @@ public class Solution {
         this.last = last;
     }
 
-    public boolean equals(Solution n) {
+    /*public boolean equals(Solution n) {
         return n.first.equals(first) && n.last.equals(last);
     }
 
     public int hashCode() {
         return 31 * first.hashCode() + last.hashCode();
-    }
+    }*/
 
     public static void main(String[] args) {
         Set<Solution> s = new HashSet<>();
         s.add(new Solution("Donald", "Duck"));
         System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Solution)) return false;
+        
+        Solution solution = (Solution) o;
+        
+        if (!Objects.equals(first, solution.first)) return false;
+        return Objects.equals(last, solution.last);
+    }
+    
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
     }
 }
