dad05f9d78bc59f807e5baa5f41316fa108a5053
Stealth <350.stealth@gmail.com>
2020-09-03 17:10:45 +0300

    Solved 2107 & add 2108

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2107/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2107/Solution.java
@@ -6,8 +6,18 @@ import java.util.Map;
 /* 
 Глубокое клонирование карты
 */
-public class Solution {
-
+public class Solution implements Cloneable{
+    
+    @Override
+    protected Solution clone() throws CloneNotSupportedException {
+//        return super.clone();
+        Solution cloneSol = new Solution();
+        for (Map.Entry<String, User> item: this.users.entrySet()) {
+            cloneSol.users.put(item.getKey(), item.getValue());
+        }
+        return cloneSol;
+    }
+    
     public static void main(String[] args) {
         Solution solution = new Solution();
         solution.users.put("Hubert", new User(172, "Hubert"));
@@ -20,6 +30,8 @@ public class Solution {
 
             System.out.println(solution.users);
             System.out.println(clone.users);
+    
+            System.out.println(solution.users.equals(clone.users));
 
         } catch (CloneNotSupportedException e) {
             e.printStackTrace(System.err);
@@ -28,13 +40,18 @@ public class Solution {
 
     protected Map<String, User> users = new LinkedHashMap();
 
-    public static class User {
+    public static class User implements Cloneable{
         int age;
         String name;
 
         public User(int age, String name) {
             this.age = age;
             this.name = name;
+        }
+    
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
         }
     }
 }
