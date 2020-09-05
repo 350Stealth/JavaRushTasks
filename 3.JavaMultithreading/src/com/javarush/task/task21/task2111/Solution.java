559a6b0934a44bf1a25764e700ec06cf4e67377a
Stealth-PC <anubis.delta@yandex.ru>
2020-09-05 14:15:27 +0300

    Solved 2111 & 2112

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2111/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2111/Solution.java
@@ -12,77 +12,65 @@ import java.util.List;
 */
 public class Solution {
     private Connection connection;
-
+    
     public Solution(Connection connection) {
         this.connection = connection;
     }
-
+    
     public List<User> getUsers() {
         String query = "select ID, DISPLAYED_NAME, LEVEL, LESSON from USER";
-
+        
         List<User> result = new LinkedList();
 
-        Statement stmt = null;
-        ResultSet rs = null;
-
-        try {
-            stmt = connection.createStatement();
-            rs = stmt.executeQuery(query);
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
             while (rs.next()) {
                 int id = rs.getInt("ID");
                 String name = rs.getString("DISPLAYED_NAME");
                 int level = rs.getInt("LEVEL");
                 int lesson = rs.getInt("LESSON");
-
+                
                 result.add(new User(id, name, level, lesson));
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
             result = null;
-        } finally {
-            if (stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
         }
         return result;
     }
-
+    
     public static class User {
         private int id;
         private String name;
         private int level;
         private int lesson;
-
+        
         public User(int id, String name, int level, int lesson) {
             this.id = id;
             this.name = name;
             this.level = level;
             this.lesson = lesson;
         }
-
+        
         @Override
         public String toString() {
             return "User{" +
-                    "id=" + id +
-                    ", name='" + name + '\'' +
-                    ", level=" + level +
-                    ", lesson=" + lesson +
-                    '}';
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", level=" + level +
+                ", lesson=" + lesson +
+                '}';
         }
     }
-
+    
+    @Override
+    protected void finalize() throws Throwable {
+        if (connection == null) return;
+        connection.close();
+        super.finalize();
+    }
+    
     public static void main(String[] args) {
-
+    
     }
 }
