c47d212cefad907013798428f2f482062cd25731
Stealth-PC <anubis.delta@yandex.ru>
2020-09-11 00:38:48 +0300

    Solved 2206 & add 2207

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
@@ -0,0 +1,60 @@
+package com.javarush.task.task22.task2207;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/* 
+Обращенные слова
+*/
+public class Solution {
+    public static List<Pair> result = new LinkedList<>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        String fileName = scanner.nextLine();
+        scanner.close();
+        
+        scanner = new Scanner(new FileReader(fileName));
+        while (scanner.hasNext()) {
+            
+        }
+        scanner.close();
+    }
+
+    public static class Pair {
+        String first;
+        String second;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Pair pair = (Pair) o;
+
+            if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+            return second != null ? second.equals(pair.second) : pair.second == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = first != null ? first.hashCode() : 0;
+            result = 31 * result + (second != null ? second.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return first == null && second == null ? "" :
+                    first == null ? second :
+                            second == null ? first :
+                                    first.compareTo(second) < 0 ? first + " " + second : second + " " + first;
+
+        }
+    }
+
+}
