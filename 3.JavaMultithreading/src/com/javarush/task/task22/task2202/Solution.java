4454cbb682aba9d81b47c89c861aee54f51dde27
Stealth-PC <anubis.delta@yandex.ru>
2020-09-10 11:05:21 +0300

    Solved 2202 & add 2203

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -6,12 +6,32 @@ package com.javarush.task.task22.task2202;
 public class Solution {
     public static void main(String[] args) {
         System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
+//        System.out.println(getPartOfString("the index of the first occurrence of the specified substring, or -1 if there is no such occurrence."));
+//        System.out.println(getPartOfString("the index of the first"));
+//        System.out.println(getPartOfString("the index of the"));
+//        System.out.println(getPartOfString("JavaRush"));
     }
 
     public static String getPartOfString(String string) {
-        return null;
+        if (string == null) throw new TooShortStringException();
+        int firstSpace = string.indexOf(" ");
+        if (firstSpace < 0) throw new TooShortStringException();
+        int secondSpace = 0;
+        String line = string;
+        for (int i = 0; i < 5; i++) {
+            int index = line.indexOf(" ");
+            if (index < 0 && i != 4) throw new TooShortStringException();
+            if (index < 0 && i == 4) {
+                index = line.length();
+            } else {
+                line = line.substring(index + 1);
+            }
+            secondSpace = secondSpace + index + 1;
+        }
+        line = string.substring(firstSpace + 1, secondSpace - 1);
+        return line;
     }
 
-    public static class TooShortStringException {
+    public static class TooShortStringException extends RuntimeException{
     }
 }
