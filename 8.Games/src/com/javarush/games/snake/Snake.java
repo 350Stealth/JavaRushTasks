beac0b3267de5e98a37bc0727644d3680d85864b
Stealth-PC <anubis.delta@yandex.ru>
2021-01-01 22:11:46 +0300

    wip snake 7/20

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -10,6 +10,8 @@ public class Snake {
     private List<GameObject> snakeParts = new ArrayList<>();
     private static final String HEAD_SIGN = "\uD83D\uDC7E";
     private static final String BODY_SIGN = "\u26AB";
+    public boolean isAlive = true;
+    private Direction direction = Direction.LEFT;
     
     public Snake(int x, int y) {
         for (int i = 0; i < 3; i++) {
@@ -19,13 +21,23 @@ public class Snake {
     }
     
     public void draw(Game game) {
+        Color snakeColor;
+        if (isAlive) {
+            snakeColor = Color.BLACK;
+        } else {
+            snakeColor = Color.RED;
+        }
         for (int i = 0; i < snakeParts.size(); i++) {
             GameObject item = snakeParts.get(i);
             if (i == 0) {
-                game.setCellValue(item.x, item.y, HEAD_SIGN);
+                game.setCellValueEx(item.x, item.y, Color.NONE, HEAD_SIGN, snakeColor, 75);
             } else {
-                game.setCellValue(item.x, item.y, BODY_SIGN);
+                game.setCellValueEx(item.x, item.y, Color.NONE, BODY_SIGN, snakeColor, 75);
             }
         }
+    }
+    
+    public void setDirection(Direction direction) {
+        this.direction = direction;
     }
 }
