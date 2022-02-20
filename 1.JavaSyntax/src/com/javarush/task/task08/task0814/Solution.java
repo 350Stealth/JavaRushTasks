16e9d917018836879bec7e3ecd8f6ced5a361df1
Stealth-PC <anubis.delta@yandex.ru>
2022-02-20 11:53:48 +0300

    wip

--- 1.JavaSyntax/src/com/javarush/task/task08/task0814/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0814/Solution.java
@@ -1,6 +1,5 @@
 package com.javarush.task.task08.task0814;
 
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,10 +12,8 @@ public class Solution {
         // напишите тут ваш код
         Set<Integer> set = new HashSet<>();
         
-        /*Date date = new Date();*/
-        
         for (int i = 0; i < 20; i++) {
-            if (!set.add(((int) (/*date.getSeconds()*/50 * Math.random()))))
+            if (!set.add(((int) (50 * Math.random()))))
                 i--;
         }
         return set;
@@ -28,8 +25,6 @@ public class Solution {
         
         for (Integer buf : set
         ) {
-            /*if (buf > 10)
-                set.remove(buf);*/
             if (buf > 10) bufSet.add(buf);
         }
         
@@ -39,21 +34,6 @@ public class Solution {
     }
     
     public static void main(String[] args) {
-        
-        /*Set<Integer> mySet = createSet();
-        
-        for (Integer buf : mySet
-        ) {
-            System.out.print(buf + " ");
-        }
-        System.out.println();
-        
-        Set<Integer> mySet2 = removeAllNumbersGreaterThan10(mySet);
-        
-        for (Integer buf : mySet2
-        ) {
-            System.out.print(buf + " ");
-        }*/
-        
+    
     }
 }
--- 1.JavaSyntax/src/com/javarush/task/task08/task0816/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0816/Solution.java
@@ -3,7 +3,10 @@ package com.javarush.task.task08.task0816;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 /* 
 Добрая Зинаида и летние каникулы
@@ -13,56 +16,43 @@ public class Solution {
     public static Map<String, Date> createMap() throws ParseException {
         DateFormat dateFormat = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
         Map<String, Date> map = new HashMap<>();
-        map.put("Сталлоне0", dateFormat.parse("MAY 1 2012"));
-        map.put("Сталлоне1", dateFormat.parse("JUNE 1 2012"));
-        map.put("Сталлоне2", dateFormat.parse("JULY 1 2012"));
-        map.put("Сталлоне3", dateFormat.parse("MARCH 1 2012"));
-        map.put("Сталлоне4", dateFormat.parse("JULY 1 2012"));
-        map.put("Сталлоне5", dateFormat.parse("MAY 1 2012"));
-        map.put("Сталлоне6", dateFormat.parse("MAY 1 2012"));
-        map.put("Сталлоне7", dateFormat.parse("JUNE 1 2012"));
-        map.put("Сталлоне8", dateFormat.parse("MAY 1 2012"));
-        map.put("Сталлоне9", dateFormat.parse("MAY 1 2012"));
-        
+        map.put("Смирнов", dateFormat.parse("JANUARY 1 2012"));
+        map.put("Смирнов2", dateFormat.parse("FEBRUARY 1 2012"));
+        map.put("Смирнов3", dateFormat.parse("MARCH 1 2012"));
+        map.put("Смирнов4", dateFormat.parse("APRIL 1 2012"));
+        map.put("Смирнов5", dateFormat.parse("MAY 1 2012"));
+        map.put("Смирнов6", dateFormat.parse("JUNE 1 2012"));
+        map.put("Смирнов7", dateFormat.parse("JULY 1 2012"));
+        map.put("Смирнов8", dateFormat.parse("AUGUST 1 2012"));
+        map.put("Смирнов9", dateFormat.parse("SEPTEMBER 1 2012"));
+        map.put("Смирнов10", dateFormat.parse("OCTOBER 1 2012"));
+
         //напишите тут ваш код
-        
         return map;
     }
-    
+
     public static void removeAllSummerPeople(Map<String, Date> map) {
         //напишите тут ваш код
         Map<String, Date> bufMap = new HashMap<>();
-    
-        bufMap.putAll(map);
-        
-        for (Map.Entry<String, Date> pair : bufMap.entrySet()
-        ) {
-            if ((pair.getValue().getMonth() >= 5) && (pair.getValue().getMonth() <= 7))
-                map.remove(pair.getKey());
+        for (Map.Entry<String, Date> item: map.entrySet()) {
+            int buf = item.getValue().getMonth();
+            if (buf >= 5 && buf <= 7) {
+                bufMap.put(item.getKey(), item.getValue());
+            }
+        }
+        for (Map.Entry<String, Date> item: bufMap.entrySet()) {
+            map.remove(item.getKey());
         }
     }
-    
-    public static void main(String[] args) throws ParseException {
-        /*
-        DateFormat dateFormat = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
-        
-        Date date = dateFormat.parse("JUNE 1 2012");
-        
-        System.out.println(date.getMonth());
-        
-        Map<String, Date> myMap = createMap();
-        
-        for (Map.Entry<String, Date> pair : myMap.entrySet()
-        ) {
-            System.out.println(pair.getKey() + " - " + pair.getValue());
+
+    public static void main(String[] args) {
+        Map<String, Date> mapa = new HashMap<>();
+        try {
+            mapa = createMap();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        
-        System.out.println();
-        
-        removeAllSummerPeople(myMap);
-        for (Map.Entry<String, Date> pair : myMap.entrySet()
-        ) {
-            System.out.println(pair.getKey() + " - " + pair.getValue());
-        }*/
+        removeAllSummerPeople(mapa);
+        System.out.println(mapa);
     }
 }
